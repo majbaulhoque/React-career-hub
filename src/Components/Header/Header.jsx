45e0674945c0efc675f364932bfcd2a679b68cd5
majbaulhoque <majba9399@gmail.com>
2024-05-09 21:03:40 +0600

    Set Page Layout and Display Featured Jobs in card

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
                         <li><a>Item 3</a></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">Career Hub</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
@@ -37,7 +37,7 @@ const Header = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <a className="btn">Apply Now</a>
             </div>
         </div>
     );
